a5dbdc18d3c15d85cb3e60d0699c56cd319d81bf
Adrian Föder <adrian@foeder.de>
2012-02-14 15:02:57 +0100

    Initial commit

---
+++ Classes/Package.php
@@ -0,0 +1,22 @@
+<?php
+namespace Assetic;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "Assetic".                    *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ *  of the License, or (at your option) any later version.                *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use \TYPO3\FLOW3\Package\Package as BasePackage;
+
+/**
+ * The Assetic Package
+ */
+class Package extends BasePackage {
+}
+
+?>
